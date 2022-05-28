6a15416ed921e74dabced74bb2b425d10567ab9c
Marcos Castelo <marcoscastelo@Marcoss-MacBook-Air.local>
2022-05-28 20:57:39 -0300

    window-sliding

---
+++ src/window-sliding/max-consecutives-ones/index.ts
@@ -0,0 +1,15 @@
+export const getMaxConsecutiveOnes = (numbers: number[], k: number): number => {
+  let leftPoint = 0;
+
+  for (let rightPoint = 0; rightPoint < numbers.length; rightPoint++) {
+    k -= 1 - numbers[rightPoint];
+
+    if (k < 0) {
+      k += 1 - numbers[leftPoint];
+
+      leftPoint++;
+    }
+
+    return rightPoint - leftPoint + 1;
+  }
+}
